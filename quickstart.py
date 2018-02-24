0078420a656531579d2c3713a7aab8b046bafedd
Harvinder <harvinder.power@gmail.com>
2018-02-24 13:09:38 +0000

    Changed to write access, able to add events to calendar

--- quickstart.py
+++ quickstart.py
@@ -17,7 +17,7 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
@@ -72,6 +72,39 @@ def main():
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
+
+    event = {
+      'summary': 'TEST EVENT',
+      'location': 'TEST LOCATION',
+      'description': 'TEST DESCRIPTION',
+      'start': {
+        'dateTime': '2018-02-24T09:00:00-07:00',
+        'timeZone': 'GMT',
+      },
+      'end': {
+        'dateTime': '2018-02-25T17:00:00-07:00',
+        'timeZone': 'GMT',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=2'
+      ],
+      'attendees': [
+        {'email': 'lpage@example.com'},
+        {'email': 'sbrin@example.com'},
+      ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    print (event.get('htmlLink'))
+    print "Event(s) successfully added to calendar"
+
 
 
 if __name__ == '__main__':
